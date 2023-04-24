4a716a6e43623136b80367a51f152aefc3e1b907
Toma Bogdan <bogdantoma2000@gmail.com>
2023-04-24 21:19:10 +0300

    tst

--- photogallery.js
+++ photogallery.js
@@ -1,27 +1,68 @@
-function getImages(imageUrl, containerSelector) {
+/*function getImages(sectionSelector, imageSrc, dataLightbox) {
+
     // get a reference to the container element where the images will be added
-    let container = document.querySelector(containerSelector);
-  
+    let container = document.querySelector(`#${sectionSelector}`);
+    // define the URL of the image folder on your server
+    let imageUrl = imageSrc;
+    let lightbox = dataLightbox;
     // make a request to the server to get a list of all the image files in the folder
     fetch(imageUrl)
-      .then(response => response.text())
-      .then(html => {
-        // create a temporary div element to hold the HTML returned by the server
-        let tempDiv = document.createElement('div');
-        tempDiv.innerHTML = html;
-  
-        // select all the <a> elements in the temporary div that link to image files
-        let imageLinks = tempDiv.querySelectorAll('a[href$=".jpg"], a[href$=".png"], a[href$=".gif"]');
-  
-        // loop through the <a> elements and dynamically generate HTML code for each image
-        for (let i = 0; i < imageLinks.length; i++) {
-          let imageSrc = imageLinks[i].getAttribute('href');
-          let imageHtml = '<img src="' + imageSrc + '">';
-          container.innerHTML += imageHtml;
-        };
-      });
-  };
-  
-  // call the getImages function with the URL of the image folder and the selector for the container element
-  getImages('https://fereastrarelax.ro/imgsite/imgsite-galerie/Cabana/', '#image-container');
-  
+        .then(response => response.text())
+        .then(html => {
+            // create a temporary div element to hold the HTML returned by the server
+            let tempDiv = document.createElement('div');
+            tempDiv.innerHTML = html;
+
+            // select all the <a> elements in the temporary div that link to image files
+            let imageLinks = tempDiv.querySelectorAll('a[href$=".jpg"], a[href$=".png"], a[href$=".gif"]');
+
+            // loop through the <a> elements and dynamically generate HTML code for each image
+            for (let i = 0; i < imageLinks.length; i++) {
+                let imageSrc = imageLinks[i].getAttribute('href');
+                let imageHtml = '<a href="' + imageSrc + '" data-lightbox= "'+ lightbox +'" ><img src="' + imageSrc + '"></a>';
+                container.innerHTML += imageHtml;
+                console.log(imageHtml);
+            };
+        });
+};
+
+window.addEventListener('load', function() {
+getImages('g-sct-HanulAncutei','imgsite/imgsite-galerie/Hanul\ Ancutei/', 'HanulAncutei');
+getImages('g-sct-cabana','https://fereastrarelax.ro/imgsite/imgsite-galerie/Cabana/', 'cabana');
+getImages('g-sct-others','imgsite/imgsite-galerie/OthersImg', 'others');
+}); */
+function getImages(sectionSelector, imageUrl, dataLightbox) {
+    let lightbox = dataLightbox;
+    let imageSrc = imageUrl;
+    // get a reference to the container element where the images will be added
+    let container = document.querySelector(`#${sectionSelector}`);
+
+    // define the URL of the image folder on your server
+    let imageUrl = 'https://fereastrarelax.ro/imgsite/imgsite-galerie/Cabana/';
+
+    // make a request to the server to get a list of all the image files in the folder
+    fetch(imageUrl)
+        .then(response => response.text())
+        .then(html => {
+            // create a temporary div element to hold the HTML returned by the server
+            let tempDiv = document.createElement('div');
+            tempDiv.innerHTML = html;
+
+            // select all the <a> elements in the temporary div that link to image files
+            let imageLinks = tempDiv.querySelectorAll('a[href$=".jpg"], a[href$=".png"], a[href$=".gif"]');
+
+            // loop through the <a> elements and dynamically generate HTML code for each image
+            for (let i = 0; i < imageLinks.length; i++) {
+                let imageSrc = imageUrl + imageLinks[i].getAttribute('href');
+                let imageHtml = '<a href="' + imageSrc + '" data-lightbox= "'+ lightbox +'" ><img src="' + imageSrc + '"></a>';
+                container.innerHTML += imageHtml;
+            }
+        })
+        .catch(error => {
+            console.error('Error fetching images:', error);
+        });
+};
+
+getImages('g-sct-HanulAncutei','imgsite/imgsite-galerie/Hanul\ Ancutei/', 'HanulAncutei');
+getImages('g-sct-cabana','https://fereastrarelax.ro/imgsite/imgsite-galerie/Cabana/', 'cabana');
+getImages('g-sct-others','imgsite/imgsite-galerie/OthersImg', 'others');
